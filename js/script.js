7727ad9dc757892b09671e450954f534b20ee4e7
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-02-28 10:49:19 +0100

    exercise correction + bonus add

--- js/script.js
+++ js/script.js
@@ -19,17 +19,17 @@ let mailInputEl = document.getElementById("mailInput");
 let loginButtonEl = document.getElementById("loginButton");
 
 // Creo la lista delle email che consentono l'accesso!
-let verifiedEmails = ["pincopallino@gmail.com", "francescofalanga@gmail.com", "vincenzovincenzini@gmail.com", "martalosito@gmail.com", "marcocarta@gmail.com", "marcomengoni@gmail.com"];
+const verifiedEmails = ["pincopallino@gmail.com", "francescofalanga@gmail.com", "vincenzovincenzini@gmail.com", "martalosito@gmail.com", "marcocarta@gmail.com", "marcomengoni@gmail.com"];
 
 console.log(verifiedEmails);
 
+// Creo una variabile in modo tale da non printare in pagina tutti i valori
+let commonVar = false;
 
 
 // Aggiungo un Evento al click del BUTTON
 loginButtonEl.addEventListener("click", function() {
 
-    // Creo una variabile in modo tale da non printare in pagina tutti i valori
-    let commonVar = false;
 
     for (let i = 0; i < verifiedEmails.length; i++) {
         // accesso consentito
@@ -38,20 +38,15 @@ loginButtonEl.addEventListener("click", function() {
             console.log("Accesso Consentito");
             commonVar = true; 
 
-        // accesso negato
-        } else {
-            
-            console.log("Accesso Negato");
-
-        }
-
-        // stampo in pagina il risultato finale
-        if (commonVar) {
-            document.getElementById("accessGranted").innerText = "Accesso Consentito";
-        } else {
-            document.getElementById("accessGranted").innerText = "Accesso Negato";
         }
         
+    }
+    
+    // stampo in pagina il risultato finale
+    if (commonVar) {
+        document.getElementById("accessGranted").innerText = "Accesso Consentito";
+    } else {
+        document.getElementById("accessGranted").innerText = "Accesso Negato";
     }
 
 })
@@ -76,6 +71,12 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 
+// BONUS creo un array e assegno la src delle immagini, creando poi due variabili per utente e pc
+let diceImgs = ["img/dice1.png", "img/dice2.png", "img/dice3.png", "img/dice4.png", "img/dice5.png", "img/dice6.png"]
+const diceImgUserEl = document.getElementById("diceImgUser");
+const diceImgPcEl = document.getElementById("diceImgPc");
+
+
 // creo le variabili dell'utente e del computer
 let userNumberEl = document.getElementById("userNumber");
 let pcNumberEl = document.getElementById("pcNumber");
@@ -87,8 +88,8 @@ let numberGeneratorButtonEl = document.getElementById("numberGeneratorButton");
 numberGeneratorButtonEl.addEventListener("click", function() {
     
     // Creo due variabili per generare i numeri casuali
-    let x = Math.floor((Math.random() * 6) + 1);
-    let y = Math.floor((Math.random() * 6) + 1);
+    const x = Math.floor((Math.random() * 6) + 1);
+    const y = Math.floor((Math.random() * 6) + 1);
     
     
     // Assegno entrambe le variabili
@@ -107,12 +108,21 @@ numberGeneratorButtonEl.addEventListener("click", function() {
     // imposto il risultato finale in pagina
     if (userNumberEl > pcNumberEl) {
         
-        document.getElementById("victory").innerText = "HAI VINTO!"
+        document.getElementById("results").innerText = "HAI VINTO!"
+    
+    } else if (userNumberEl < pcNumberEl) {
+    
+        document.getElementById("results").innerText = "IL COMPUTER TI HA BATTUTO!";
     
     } else {
-    
-        document.getElementById("victory").innerText = "IL COMPUTER TI HA BATTUTO!";
-    
+
+        document.getElementById("results").innerText = "PAREGGIO!";
+
     }
 
+    // imposto l'output delle img per visualizzare i dadi correttamente in pagina
+    diceImgUserEl.src = diceImgs[userNumberEl - 1];
+    diceImgPcEl.src = diceImgs[pcNumberEl - 1];
+
 })
+
